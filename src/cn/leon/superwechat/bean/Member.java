d25aa9f79083a3c07ac1188bdf62c237ff85173b
leonFighting <379029170@qq.com>
2016-05-19 16:18:37 +0800

    User类中增加header,get,set,equals,hashCode方法,修改member,equals方法

--- src/cn/leon/superwechat/bean/Member.java
+++ src/cn/leon/superwechat/bean/Member.java
@@ -5,115 +5,119 @@ package cn.leon.superwechat.bean;
  * Member entity. @author MyEclipse Persistence Tools
  */
 public class Member extends User implements java.io.Serializable {
-	private static final long serialVersionUID = 6913484375413577556L;
+    private static final long serialVersionUID = 6913484375413577556L;
 
-	// Fields
+    // Fields
 
-	/**
-	 * 
-	 */
-	private Integer mmemberId;
-	private Integer mmemberUserId;
-	private String mmemberUserName;
-	private Integer mmemberGroupId;
-	private String mmemberGroupHxid;
-	private Integer mmemberPermission;
+    /**
+     *
+     */
+    private Integer mmemberId;
+    private Integer mmemberUserId;
+    private String mmemberUserName;
+    private Integer mmemberGroupId;
+    private String mmemberGroupHxid;
+    private Integer mmemberPermission;
 
-	// Constructors
+    // Constructors
 
-	/** default constructor */
-	public Member() {
-	}
+    /**
+     * default constructor
+     */
+    public Member() {
+    }
 
-	/** full constructor */
-	public Member(Integer MMemberUserId, String MMemberUserName,
-			Integer MMemberGroupId, String MMemberGroupHxid,
-			Integer MMemberPermission) {
-		this.mmemberUserId = MMemberUserId;
-		this.mmemberUserName = MMemberUserName;
-		this.mmemberGroupId = MMemberGroupId;
-		this.mmemberGroupHxid = MMemberGroupHxid;
-		this.mmemberPermission = MMemberPermission;
-	}
+    /**
+     * full constructor
+     */
+    public Member(Integer MMemberUserId, String MMemberUserName,
+                  Integer MMemberGroupId, String MMemberGroupHxid,
+                  Integer MMemberPermission) {
+        this.mmemberUserId = MMemberUserId;
+        this.mmemberUserName = MMemberUserName;
+        this.mmemberGroupId = MMemberGroupId;
+        this.mmemberGroupHxid = MMemberGroupHxid;
+        this.mmemberPermission = MMemberPermission;
+    }
 
-	// Property accessors
-	public Integer getMMemberId() {
-		return this.mmemberId;
-	}
+    // Property accessors
+    public Integer getMMemberId() {
+        return this.mmemberId;
+    }
 
-	public void setMMemberId(Integer MMemberId) {
-		this.mmemberId = MMemberId;
-	}
+    public void setMMemberId(Integer MMemberId) {
+        this.mmemberId = MMemberId;
+    }
 
-	public Integer getMMemberUserId() {
-		return this.mmemberUserId;
-	}
+    public Integer getMMemberUserId() {
+        return this.mmemberUserId;
+    }
 
-	public void setMMemberUserId(Integer MMemberUserId) {
-		this.mmemberUserId = MMemberUserId;
-	}
+    public void setMMemberUserId(Integer MMemberUserId) {
+        this.mmemberUserId = MMemberUserId;
+    }
 
-	public String getMMemberUserName() {
-		return this.mmemberUserName;
-	}
+    public String getMMemberUserName() {
+        return this.mmemberUserName;
+    }
 
-	public void setMMemberUserName(String MMemberUserName) {
-		this.mmemberUserName = MMemberUserName;
-	}
+    public void setMMemberUserName(String MMemberUserName) {
+        this.mmemberUserName = MMemberUserName;
+    }
 
-	public Integer getMMemberGroupId() {
-		return this.mmemberGroupId;
-	}
+    public Integer getMMemberGroupId() {
+        return this.mmemberGroupId;
+    }
 
-	public void setMMemberGroupId(Integer MMemberGroupId) {
-		this.mmemberGroupId = MMemberGroupId;
-	}
+    public void setMMemberGroupId(Integer MMemberGroupId) {
+        this.mmemberGroupId = MMemberGroupId;
+    }
 
-	public String getMMemberGroupHxid() {
-		return this.mmemberGroupHxid;
-	}
+    public String getMMemberGroupHxid() {
+        return this.mmemberGroupHxid;
+    }
 
-	public void setMMemberGroupHxid(String MMemberGroupHxid) {
-		this.mmemberGroupHxid = MMemberGroupHxid;
-	}
+    public void setMMemberGroupHxid(String MMemberGroupHxid) {
+        this.mmemberGroupHxid = MMemberGroupHxid;
+    }
 
-	public Integer getMMemberPermission() {
-		return this.mmemberPermission;
-	}
+    public Integer getMMemberPermission() {
+        return this.mmemberPermission;
+    }
 
-	public void setMMemberPermission(Integer MMemberPermission) {
-		this.mmemberPermission = MMemberPermission;
-	}
+    public void setMMemberPermission(Integer MMemberPermission) {
+        this.mmemberPermission = MMemberPermission;
+    }
 
-	@Override
-	public String toString() {
-		return "Member [MMemberId=" + mmemberId + ", MMemberUserId="
-				+ mmemberUserId + ", MMemberUserName=" + mmemberUserName
-				+ ", MMemberGroupId=" + mmemberGroupId + ", MMemberGroupHxid="
-				+ mmemberGroupHxid + ", MMemberPermission=" + mmemberPermission
-				+ "]";
-	}
+    @Override
+    public String toString() {
+        return "Member [MMemberId=" + mmemberId + ", MMemberUserId="
+                + mmemberUserId + ", MMemberUserName=" + mmemberUserName
+                + ", MMemberGroupId=" + mmemberGroupId + ", MMemberGroupHxid="
+                + mmemberGroupHxid + ", MMemberPermission=" + mmemberPermission
+                + "]";
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-		Member member = (Member) o;
+        Member member = (Member) o;
 
-		if (!mmemberUserId.equals(member.mmemberUserId)) return false;
-		if (!mmemberUserName.equals(member.mmemberUserName)) return false;
-		if (!mmemberGroupId.equals(member.mmemberGroupId)) return false;
-		return mmemberGroupHxid.equals(member.mmemberGroupHxid);
+        return (mmemberUserId.equals(member.mmemberUserId)
+                && mmemberUserName.equals(member.mmemberUserName))
+                && mmemberGroupId.equals(member.mmemberGroupId)
+                && mmemberGroupHxid.equals(member.mmemberGroupHxid);
 
-	}
+    }
 
-	@Override
-	public int hashCode() {
-		int result = mmemberUserId.hashCode();
-		result = 31 * result + mmemberUserName.hashCode();
-		result = 31 * result + mmemberGroupId.hashCode();
-		result = 31 * result + mmemberGroupHxid.hashCode();
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        int result = mmemberUserId.hashCode();
+        result = 31 * result + mmemberUserName.hashCode();
+        result = 31 * result + mmemberGroupId.hashCode();
+        result = 31 * result + mmemberGroupHxid.hashCode();
+        return result;
+    }
 }
--- src/cn/leon/superwechat/bean/User.java
+++ src/cn/leon/superwechat/bean/User.java
@@ -5,93 +5,121 @@ package cn.leon.superwechat.bean;
  * User entity. @author MyEclipse Persistence Tools
  */
 public class User extends Location implements java.io.Serializable {
-	private static final long serialVersionUID = 6848921231724157394L;
+    private static final long serialVersionUID = 6848921231724157394L;
 
-	// Fields 
+    // Fields
 
-	/**
-	 * 
-	 */
-	private Integer muserId;
-	private String muserName;
-	private String muserPassword;
-	private String muserNick;
-	private Integer muserUnreadMsgCount;
+    /**
+     *
+     */
+    private Integer muserId;
+    private String muserName;
+    private String muserPassword;
+    private String muserNick;
+    private Integer muserUnreadMsgCount;
+    private String header;
+    // Constructors
 
-	// Constructors
+    /**
+     * default constructor
+     */
+    public User() {
+    }
 
-	/** default constructor */
-	public User() {
-	}
-	
-	public User(boolean result, int msg) {
-		this.setResult(result);
-		this.setMsg(msg);
-	}
+    public User(boolean result, int msg) {
+        this.setResult(result);
+        this.setMsg(msg);
+    }
 
-	/** minimal constructor */
-	public User(Integer MUserId, String MUserName, String MUserPassword, String MUserNick) {
-		this.muserId = MUserId;
-		this.muserName = MUserName;
-		this.muserPassword = MUserPassword;
-		this.muserNick = MUserNick;
-	}
+    /**
+     * minimal constructor
+     */
+    public User(Integer MUserId, String MUserName, String MUserPassword, String MUserNick) {
+        this.muserId = MUserId;
+        this.muserName = MUserName;
+        this.muserPassword = MUserPassword;
+        this.muserNick = MUserNick;
+    }
 
-	/** full constructor */
-	public User(Integer MUserId, String MUserName, String MUserPassword, String MUserNick,
-			Integer MUserUnreadMsgCount) {
-		this(MUserId, MUserName, MUserPassword, MUserNick);
-		this.muserUnreadMsgCount = MUserUnreadMsgCount;
-	}
+    /**
+     * full constructor
+     */
+    public User(Integer MUserId, String MUserName, String MUserPassword, String MUserNick,
+                Integer MUserUnreadMsgCount) {
+        this(MUserId, MUserName, MUserPassword, MUserNick);
+        this.muserUnreadMsgCount = MUserUnreadMsgCount;
+    }
 
-	// Property accessors
-	public Integer getMUserId() {
-		return this.muserId;
-	}
+    // Property accessors
+    public Integer getMUserId() {
+        return this.muserId;
+    }
 
-	public void setMUserId(Integer MUserId) {
-		this.muserId = MUserId;
-	}
+    public void setMUserId(Integer MUserId) {
+        this.muserId = MUserId;
+    }
 
-	public String getMUserName() {
-		return this.muserName;
-	}
+    public String getMUserName() {
+        return this.muserName;
+    }
 
-	public void setMUserName(String MUserName) {
-		this.muserName = MUserName;
-	}
+    public void setMUserName(String MUserName) {
+        this.muserName = MUserName;
+    }
 
-	public String getMUserPassword() {
-		return this.muserPassword;
-	}
+    public String getMUserPassword() {
+        return this.muserPassword;
+    }
 
-	public void setMUserPassword(String MUserPassword) {
-		this.muserPassword = MUserPassword;
-	}
+    public void setMUserPassword(String MUserPassword) {
+        this.muserPassword = MUserPassword;
+    }
 
-	public String getMUserNick() {
-		return this.muserNick;
-	}
+    public String getMUserNick() {
+        return this.muserNick;
+    }
 
-	public void setMUserNick(String MUserNick) {
-		this.muserNick = MUserNick;
-	}
+    public void setMUserNick(String MUserNick) {
+        this.muserNick = MUserNick;
+    }
 
-	public Integer getMUserUnreadMsgCount() {
-		return this.muserUnreadMsgCount;
-	}
+    public Integer getMUserUnreadMsgCount() {
+        return this.muserUnreadMsgCount;
+    }
 
-	public void setMUserUnreadMsgCount(Integer MUserUnreadMsgCount) {
-		this.muserUnreadMsgCount = MUserUnreadMsgCount;
-	}
+    public void setMUserUnreadMsgCount(Integer MUserUnreadMsgCount) {
+        this.muserUnreadMsgCount = MUserUnreadMsgCount;
+    }
 
-	@Override
-	public String toString() {
-		return "User [MUserId=" + muserId + ", MUserName=" + muserName
-				+ ", MUserPassword=" + muserPassword + ", MUserNick="
-				+ muserNick + ", MUserUnreadMsgCount=" + muserUnreadMsgCount
-				+ "]";
-	}
-	
+    public String getHeader() {
+        return header;
+    }
 
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    @Override
+    public String toString() {
+        return "User [MUserId=" + muserId + ", MUserName=" + muserName
+                + ", MUserPassword=" + muserPassword + ", MUserNick="
+                + muserNick + ", MUserUnreadMsgCount=" + muserUnreadMsgCount
+                + ",header=" + header + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        return muserName.equals(user.muserName);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return muserName.hashCode();
+    }
 }
